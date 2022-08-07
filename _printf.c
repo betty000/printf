43072b750e1f331a9ae336a568ecdd3de34cfd31
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-08-07 10:29:45 -0700

    task 0 correction

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf -  function that produces output according to a format
  * @format: specify the text format
@@ -7,64 +9,51 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int (*func)(va_list);
-	int i = 0;
-	int len_arg = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
-	va_start(ap, format);
-	while (format[i])
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] == '%' && format[i + 1] == '\0')
+		if (format[i] != '%')
 		{
-			return (-1);
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			printed_chars++;
 		}
-		else if (format[i] == '%' && format[i + 1] == '%')
+		else
 		{
-			_putchar('%');
-			len_arg++;
-			i += 2;
-			continue;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
-		else if (format[i] == '%')
-		{
-			func = get_op_func(&format[i]);
-			if (func != NULL)
-			{
-				len_arg += func(ap);
-				i += 2;
-				continue;
-			}
-			else
-			{
-				if (format[i] == '%' && format[i + 1] == '\0')
-				{
-					len_arg++;
-					_putchar(format[i]);
-				}
-				else
-				{
-					len_arg++;
-					_putchar(format[i]);
-				}
-			}
-		}
-		else if (format[i] == 92 && format[i + 1] == 110)
-		{
-			_putchar('\n');
-			break;
-		}
-		else if (format[i] != '\0')
-		{
-			_putchar(format[i]);
-			len_arg++;
-		}
-		i++;
 	}
-	va_end(ap);
-	return (len_arg);
+	print_buffer(buffer, &buff_ind);
+	va_end(list);
+	return (printed_chars);
+}
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
 }
--- main.h
+++ main.h
@@ -1,5 +1,21 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#define BUFF_SIZE 1024
+/**
+ * struct fmt - Struct op
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
 
-int _printf(const char *format, ...;
+/**
+ * typedef struct fmt fmt_t - Struct op
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+int _printf(const char *format, ...);
 #endif 
